1f1d311778e18baeb497149c013e195eaf3be434
Nick <107044099+ndjogo@users.noreply.github.com>
2023-06-03 12:58:47 +0100

    Updated classes for data extraction

--- data_extraction.py
+++ data_extraction.py
@@ -1,3 +1,33 @@
-class DataExtractor():
-    pass
+from database_utils import DatabaseConnector
+import pandas as pd
 
+
+class DataExtractor():
+    def __init__(self):
+        pass
+    def read_rds_table(self, database_connector, table_name):
+        connection = database_connector.init_db_engine()
+        cursor = connection.cursor()
+        cursor.execute(f'SELECT * FROM {table_name}')
+        data_frame = pd.DataFrame(cursor)
+        return data_frame 
+        
+        pass 
+
+
+
+if __name__ == '__main__':
+    database_connector = DatabaseConnector()
+    data_extraction = DataExtractor()
+
+    database_connector.list_db_tables() 
+    while True: 
+        try:
+            table_name = input("enter table name required: ")
+            samuel_data = data_extraction.read_rds_table(database_connector, table_name)
+            break
+        except: 
+            print('incorrect table name entered')
+            continue
+
+    print(samuel_data)
--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,41 @@
+import yaml
+import mysql.connector
+
 class DatabaseConnector():
+    def __init__(self, cred_file = 'db_creds.yaml'): 
+        self.cred_file = cred_file 
+
+    def read_db_creds(self):
+        print('reading credentials...')
+        with open(self.cred_file) as file:
+            cred = yaml.safe_load(file)
+        return cred
+    
+    def init_db_engine(self):
+        cred = self.read_db_creds()
+        print('connecting to the database....')
+        connection = mysql.connector.connect(host = cred['RDS_HOST'] , 
+                                     user = cred['RDS_USER'], 
+                                     password = cred['RDS_PASSWORD'] ,
+                                     port = str(cred['RDS_PORT']),
+                                     database = cred['RDS_DATABASE'])
+        return connection 
+    
+    def list_db_tables(self):
+        connection = self.init_db_engine() 
+        cursor = connection.cursor() 
+        print('reading existing tables....')
+        cursor.execute('SHOW TABLES')
+        for row in cursor:
+            print(row)
+    
+ 
+
     pass
+
+
+if __name__ == '__main__':
+    database_connector = DatabaseConnector()
+    database_connector.list_db_tables()
+
+
