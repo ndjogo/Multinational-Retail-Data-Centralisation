a4f2b43f3d07d66331215f13ebb038ecba1fec96
Nick <107044099+ndjogo@users.noreply.github.com>
2023-06-08 11:55:12 +0100

    Updated upload data method

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,8 @@
 import yaml
 import mysql.connector
+from sqlalchemy import create_engine
+import pandas as pd
+import datetime
 
 class DatabaseConnector():
     def __init__(self, cred_file = 'db_creds.yaml'): 
@@ -29,13 +32,42 @@ class DatabaseConnector():
         for row in cursor:
             print(row)
     
- 
+    def upload_to_db(self, pd_dataframe):
+        cred = self.read_db_creds()
+        db_url = f'mysql+mysqlconnector://{cred["RDS_USER"]}:{cred["RDS_PASSWORD"]}@{cred["RDS_HOST"]}/{cred["RDS_DATABASE"]}'
+        engine = create_engine(db_url)
+        data_columns = pd_dataframe.columns
+        with engine.connect() as connection:
+            connection.execute(f"""CREATE TABLE IF NOT EXISTS dim_users(
+                               {data_columns[0]} VARCHAR(100),
+                                 {data_columns[1]} DATE,
+                                   {data_columns[2]} VARCHAR(100),
+                                     {data_columns[3]} DATE)""")
+    
+    def upload_to_db_2(self, pd_dataframe):
+        connection = self.init_db_engine() 
+        cursor = connection.cursor() 
+        data_columns = pd_dataframe.columns
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS dim_users(
+                               {data_columns[0]} VARCHAR(100),
+                                 {data_columns[1]} DATE,
+                                   {data_columns[2]} VARCHAR(100),
+                                     {data_columns[3]} DATE)""")
+        cursor.close()
+        
+        cred = self.read_db_creds()
+        db_url = f'mysql+mysqlconnector://{cred["RDS_USER"]}:{cred["RDS_PASSWORD"]}@{cred["RDS_HOST"]}/{cred["RDS_DATABASE"]}'
+        engine = create_engine(db_url)
+        pd_dataframe.to_sql('dim_user',con = engine, if_exists = 'append', index = False)
 
-    pass
 
 
 if __name__ == '__main__':
     database_connector = DatabaseConnector()
     database_connector.list_db_tables()
+    database_connector.upload_to_db_2(pd.DataFrame([['111111111', datetime.date(1990,3,10), 'barclays', datetime.date(1995,3,10)]],columns = ['card_number', 'expiry_date', 'card_provider', 'date_payment_confirmed']))
+    
+
+
 
 
