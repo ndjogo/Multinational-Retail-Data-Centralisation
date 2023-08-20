efa7e925aa63f326cb6dcbf4fab140a448a66be2
Nick <107044099+ndjogo@users.noreply.github.com>
2023-08-20 15:55:26 +0100

    Update extract_aicore_data.py

--- extract_aicore_data.py
+++ extract_aicore_data.py
@@ -17,3 +17,13 @@ if __name__ == '__main__':
 
 
 
+    database_connector = DatabaseConnector_postgres()
+    existing_tables = database_connector.list_db_tables()
+    print(existing_tables)
+    data_extractor = DataExtractor()
+    table_data = data_extractor.read_rds_tables_postgress(database_connector).iloc[:,1:]
+    table_data = DataCleaning.clean_orders_data(table_data)
+
+    database_connector = DatabaseConnector()
+    database_connector.list_db_tables()
+    database_connector.upload_to_db_2(table_data, 'orders_table')
